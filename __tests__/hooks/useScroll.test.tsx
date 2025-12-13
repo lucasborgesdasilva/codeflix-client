730fe35e9dd7e7714d6a234eeb6bb5ca826e074c
Lucas Borges da Silva <lucasborges2812@gmail.com>
2025-12-13 14:06:26 -0300

    Primeiro teste

---
+++ __tests__/hooks/useScroll.test.tsx
@@ -0,0 +1,22 @@
+import { useScroll } from "@/app/hooks/useScroll";
+import { act, renderHook } from "@testing-library/react";
+
+describe('useScroll', () => {
+  it('should respond to scroll events', () => {
+    const { result } = renderHook(() => useScroll());
+
+    act(() => {
+      global.window.scrollY = 100;
+      global.window.dispatchEvent(new Event('scroll'));
+    });
+
+    expect(result.current.isScrolled).toBe(true);
+
+    act(() => {
+      global.window.scrollY = 0;
+      global.window.dispatchEvent(new Event('scroll'));
+    });
+
+    expect(result.current.isScrolled).toBe(false);
+  });
+});
