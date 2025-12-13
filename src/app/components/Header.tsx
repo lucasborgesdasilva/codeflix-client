89b0f320f206df920b2561283ee4ec386e2ccda9
Lucas Borges da Silva <lucasborges2812@gmail.com>
2025-12-13 02:31:35 -0300

    wip: Header Hook

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,27 @@
+'use client'; //Agora os componentes por default são server components, para usar hooks do react no lado do cliente, precisamos colocar isso no topo do arquivo.
+import { useEffect, useState } from "react";
+
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll); //cleanup to avoid memory leaks
+    }
+  }, []);
+
   return (
-    <header className="fixed top-0 z-50 flex w-full items-center justify-between p-4 lg:px-10 lg:py-6 transition-all">
+    <header className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between p-4 lg:px-10 lg:py-6 transition-all`}>
       <div className="flex items-center space-x-2 md:space-x-8">
         <img src="./logo.svg" alt="netflix" width={120} height={120} />
         <ul className="hidden md:flex md:space-x-4">
