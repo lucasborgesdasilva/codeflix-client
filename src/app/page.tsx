1470ca8329c58b774f2a8b75dcf341dbb9ea78fc
Lucas Borges da Silva <lucasborges2812@gmail.com>
2025-12-12 21:42:03 -0300

    wip: banner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,37 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="relative h-full lg:h-[140vh]">
       <main className="relative pb-24 pl-4 lg:space-y-24 lg:pl-16">
         <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
-          <div className="absolute flex flex-col left-0 top-0 -z-10 h-[95vh] w-screen bg-black"></div>
+          <div className="absolute flex flex-col left-0 top-0 -z-10 h-[95vh] w-screen bg-black">
+            <Image
+              src="/banner.png"
+              alt="Maid"
+              fill={true}
+              className="h-[65vh] object-cover object-top lg:h-[95vh]"
+            />
+          </div>
 
           <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
-            The Witcher
+            Maid
           </h1>
 
           <p className="text-shadow-md max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl">
-            Geralt of Rivia, a solitary monster hunter, struggles to find his
-            place in a world wher people often prove more wicked than beasts
+            Plagued by financial and family problems, Alex leaves behind an abusive relationship
+            and finds a job as a cleaner to support her daughter and build a future for them both.
           </p>
         </div>
 
-        <button className="bg-white text-black font-bold py-2 px-4 rounded-full w-24 md:w-32 lg:w-40">Play</button>
-        <button className="bg-gray-700 text-white font-bold py-2 px-4 rounded-full w-24 md:w-32 lg:w-40">More Info</button>
+        <div className="flex space-x-3">
+          <button className="md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-white px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5">
+            Play
+          </button>
+          <button className="md:text-xl; flex cursor-pointer items-center gap-x-2 rounded bg-gray-600 px-5 py-1.5 text-sm font-semibold text-black transition hover:opacity-75 md:px-8 md:py-2.5">
+            More Info
+          </button>
+        </div>
       </main>
     </div>
   );
