6857c218cfa39dc16c3a2139790cc52eee70c7bd
Lucas Borges da Silva <lucasborges2812@gmail.com>
2025-12-13 19:46:44 -0300

    Componente Loading

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,7 +1,16 @@
-export default function Settings() {
+import { getAppSettings } from "../lib/settings";
+
+export default async function Settings() {
+  const { theme, language } = await getAppSettings();
+
   return (
     <div>
-      <h1>Settings</h1>
+      <h1>Settings Page</h1>
+
+      <div className="border border-dashed border-red-500 p-4">
+        <p>Theme: {theme}</p>
+        <p>Language: {language}</p>
+      </div>
     </div>
   )
 }
--- src/app/settings/user/page.tsx
+++ src/app/settings/user/page.tsx
@@ -1,7 +1,17 @@
-export default function User() {
+import { getUserInfo } from "@/app/lib/settings";
+
+export default async function User() {
+  const { name, email, age } = await getUserInfo();
+
   return (
     <div>
       <h1>User Page</h1>
+
+      <div className="border border-dashed border-red-500 p-4">
+        <p>Name: {name}</p>
+        <p>email: {email}</p>
+        <p>age: {age}</p>
+      </div>
     </div>
   )
 }
