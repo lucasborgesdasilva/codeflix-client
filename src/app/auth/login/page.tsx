ce8b6b8b86514b7f5378894e8d48b0beb7e3746e
Lucas Borges da Silva <lucasborges2812@gmail.com>
2025-12-31 19:16:25 -0300

    wip: Aprimorado o layout e o estilo do formulário de login.

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,10 +1,11 @@
 export default function Login() {
   return (
-    <div style={{ backgroundImage: 'url(/background.jpg)' }}
+    <div
+      style={{ backgroundImage: 'url(/background.jpg)' }}
       className="bg-cover bg-center bg-no-repeat text-white opacity-80"
     >
       <div className="flex min-h-screen flex-col items-center justify-center py-2">
-        <form action="" className="flex w-full flex-col space-y-4 bg-[#141414]/90 px-4 py-8 shadow-lg">
+        <form action="" className="flex w-full max-w-md rounded flex-col space-y-4 bg-[#141414]/90 px-4 py-8 shadow-lg">
           <div className="flex flex-col items-center space-y-4">
             <h1 className="text-3xl font-bold">Login</h1>
             <p className="text-sm text-gray-500">
@@ -14,14 +15,38 @@ export default function Login() {
               </a>
             </p>
           </div>
-          <div>
-            <label htmlFor="email" className="mb-2 block text-sm font-medium">Email address</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="w-full rounded bg-gray-700 p-2 text-white focus:outline-none focus:ring-2 focus:ring-red-500"
-            />
+          <div className="mt-8 flex flex-col space-y-4">
+            <div className="flex flex-col space-y-1">
+              <label
+                htmlFor="email"
+                className="text-sm font-semibold text-gray-500"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                className="rounded-lg border-gray-600 bg-gray-700 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent"
+              />
+            </div>
+            <div className="flex flex-col space-y-1">
+              <label
+                htmlFor="password"
+                className="text-sm font-semibold text-gray-500"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                className="rounded-lg border-gray-600 bg-gray-700 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col-reverse space-y-2 pt-2 sm:flex-row sm:space-x-2 sm:space-y-0">
+            <button>Submit</button>
           </div>
         </form>
       </div>
