01b19814c2343e4f8fe3f72fbd982b9fb8a143b5
Lucas Borges da Silva <lucasborges2812@gmail.com>
2026-01-03 18:00:11 -0300

    feat: add movie service

---
+++ src/app/service/movie-service.ts
@@ -0,0 +1,36 @@
+import { Movie, Movies } from "../types/movie";
+import { apiRequest, RequestOptions } from "./api-request";
+
+export const getMovieById = async (id: number): Promise<Movie> => {
+  return apiRequest(`movies/${encodeURIComponent(id)}`);
+};
+
+export const getFeaturedMovie = async (id: string): Promise<Movie> => {
+  return apiRequest(`featured/${id}`)
+}
+
+export const getMoviesByGenre = async (
+  genre: string,
+  options?: RequestOptions
+): Promise<Movies> => {
+  return apiRequest(
+    `movies`,
+    { genre_like: encodeURIComponent(genre) },
+    options
+  );
+}
+
+export const searchMovies = async (
+  title: string,
+  genre: string,
+  options: RequestOptions
+): Promise<Movies> => {
+  return apiRequest(
+    `movies`,
+    {
+      title_like: encodeURIComponent(title),
+      genre_like: encodeURIComponent(genre)
+    },
+    options
+  );
+}
